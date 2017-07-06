85377ad73a0b3567ba4a3f724c8498f192ae9e30
Federico Bouzas <federicobouzas@gmail.com>
2017-07-06 19:02:46 -0300

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,9 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('timbreo', ['ionic', 'ngCordova'])
 
-        .run(function ($ionicPlatform, $rootScope) {
-            $rootScope.remoteURL = 'http://eideoos.com:5984/timbreo-merlo';
-            //$rootScope.remoteURL = ' http://secdes.sytes.net:5984/timbreo-merlo';
+        .run(function ($ionicPlatform) {
             $ionicPlatform.ready(function () {
                 if (window.cordova && window.cordova.plugins.Keyboard) {
-                    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                    // for form inputs)
                     cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-                    // Don't remove this line unless you know what you are doing. It stops the viewport
-                    // from snapping when text inputs are focused. Ionic handles this internally for
-                    // a much nicer keyboard experience.
                     cordova.plugins.Keyboard.disableScroll(true);
                 }
                 if (window.StatusBar) {
@@ -43,13 +30,16 @@ angular.module('timbreo', ['ionic', 'ngCordova'])
         })
 
         .controller('LoginController', function ($ionicPopup, $rootScope, $scope, $state, PouchDB) {
-            var dbLocal = new PouchDB('timbreo-merlo');
-            dbLocal.destroy();
+            $scope.partidos = ["merlo", "campana"];
             $rootScope.user = {};
+            $rootScope.partido = "";
             $scope.login = function () {
                 var errores = [];
                 if (!$rootScope.user.identificacion || $rootScope.user.identificacion == "") {
                     errores.push("- Identificación requerida.");
+                }
+                if ($rootScope.partido == "") {
+                    errores.push("- Seleccione un Partido.");
                 }
                 if (errores.length) {
                     $ionicPopup.alert({
@@ -59,19 +49,89 @@ angular.module('timbreo', ['ionic', 'ngCordova'])
                     });
                     return;
                 }
-                $state.go("timbreo");
+                (new PouchDB('timbreo-' + $rootScope.partido)).destroy().then(function () {
+                    $state.go("timbreo");
+                });
+            };
+            $scope.seleccionarPartido = function (partido) {
+                $rootScope.partido = partido;
             };
             navigator.geolocation.getCurrentPosition(function (position) {
                 console.log("ok GEO");
+                var geocoder = new google.maps.Geocoder;
+                //var latlngMerlo = {lat: -34.6751447, lng: -58.7604686};
+                //var latlngCampana = {lat: -34.1633346, lng: -58.9592643};
+                var latlng = {lat: parseFloat(position.coords.latitude), lng: parseFloat(position.coords.longitude)};
+                geocoder.geocode({'location': latlng}, function (results, status) {
+                    if (status === 'OK') {
+                        for (var i in results) {
+                            if (results[i].types[0] == "administrative_area_level_2") {
+                                var partido = results[i].address_components[0].long_name.toLowerCase();
+                                if ($scope.partidos.indexOf(partido) !== -1) {
+                                    $rootScope.partido = partido;
+                                    $rootScope.$apply();
+                                }
+                                break;
+                            }
+                        }
+                    }
+                });
             }, function (error) {
                 console.log('code: ' + error.code + '\nmessage: ' + error.message + '\n');
             });
         })
 
-        .controller('TimbreoController', function ($ionicScrollDelegate, $rootScope, $scope, PouchDB, $ionicPopup, $timeout, $state) {
-            var dbLocal = new PouchDB('timbreo');
-            PouchDB.replicate(dbLocal, $rootScope.remoteURL, {live: true, retry: true});
-            $scope.preguntas = {
+        .controller('TimbreoController', function ($ionicScrollDelegate, $rootScope, $scope, PouchDB,
+                $ionicPopup, $timeout, $state, PreguntasMerlo, PreguntasCampana) {
+            var dbLocal = new PouchDB('timbreo-' + $rootScope.partido);
+            PouchDB.replicate(dbLocal, 'https://couchdb.timbrea.me/timbreo-' + $rootScope.partido + '-dev', {live: true, retry: true});
+            $scope.preguntas = $rootScope.partido == "merlo" ? PreguntasMerlo : PreguntasCampana;
+            var resetForm = function () {
+                $scope.formulario = {user: $rootScope.user, position: {}, respuestas: {}};
+            };
+            $scope.seleccionar = function (numero, valor) {
+                $scope.formulario.respuestas[numero] = valor;
+            };
+            $scope.seleccionarMultiple = function (numero, seccion, valor) {
+                $scope.formulario.respuestas[numero] = $scope.formulario.respuestas[numero] || {};
+                $scope.formulario.respuestas[numero][seccion] = valor;
+            };
+            $scope.exit = function () {
+                $rootScope.user = {};
+                $state.go("login");
+            };
+            $scope.capitalizeFirstLetter = function (string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            };
+            $scope.guardar = function () {
+                var formulario = $scope.formulario;
+                formulario.time = new Date().getTime();
+                console.log("start GEO");
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    console.log("ok GEO");
+                    formulario.position.latitude = position.coords.latitude;
+                    formulario.position.longitude = position.coords.longitude;
+                    dbLocal.post(formulario);
+                }, function (error) {
+                    console.log('code: ' + error.code + '\nmessage: ' + error.message + '\n');
+                    dbLocal.post(formulario);
+                });
+                var alertPopup = $ionicPopup.alert({
+                    title: '¡Formulario guardado!',
+                    template: '<div class="thumbs-up"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></div>',
+                    buttons: []
+                });
+                $ionicScrollDelegate.scrollTop();
+                $timeout(function () {
+                    alertPopup.close();
+                }, 3000);
+                resetForm();
+            };
+            resetForm();
+        })
+
+        .factory('PreguntasMerlo', function () {
+            return {
                 1: {
                     tipo: 'simple.estado',
                     texto: '¿Conoce por los medios a la Gobernadora María Eugenia Vidal?',
@@ -121,45 +181,16 @@ angular.module('timbreo', ['ionic', 'ngCordova'])
                     tipo: 'simple.estado',
                     texto: '¿Y a nivel Nacional?',
                     opciones: {"Si": "Si", "No": "No"}
-                },
+                }
             };
-            var resetForm = function () {
-                $scope.formulario = {user: $rootScope.user, position: {}, respuestas: {}};
+        })
+
+        .factory('PreguntasCampana', function () {
+            return {
+                1: {
+                    tipo: 'simple.estado',
+                    texto: '¿Conoce por los medios a la Gobernadora María Eugenia Vidal?',
+                    opciones: {"Si": "Si", "No": "No"}
+                }
             };
-            $scope.seleccionar = function (numero, valor) {
-                $scope.formulario.respuestas[numero] = valor;
-            };
-            $scope.seleccionarMultiple = function (numero, seccion, valor) {
-                $scope.formulario.respuestas[numero] = $scope.formulario.respuestas[numero] || {};
-                $scope.formulario.respuestas[numero][seccion] = valor;
-            };
-            $scope.exit = function () {
-                $rootScope.user = {};
-                $state.go("login");
-            };
-            $scope.guardar = function () {
-                var formulario = $scope.formulario;
-                formulario.time = new Date().getTime();
-                console.log("start GEO");
-                navigator.geolocation.getCurrentPosition(function (position) {
-                    console.log("ok GEO");
-                    formulario.position.latitude = position.coords.latitude;
-                    formulario.position.longitude = position.coords.longitude;
-                    dbLocal.post(formulario);
-                }, function (error) {
-                    console.log('code: ' + error.code + '\nmessage: ' + error.message + '\n');
-                    dbLocal.post(formulario);
-                });
-                var alertPopup = $ionicPopup.alert({
-                    title: '¡Formulario guardado!',
-                    template: '<div class="thumbs-up"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></div>',
-                    buttons: []
-                });
-                $ionicScrollDelegate.scrollTop();
-                $timeout(function () {
-                    alertPopup.close();
-                }, 3000);
-                resetForm();
-            };
-            resetForm();
         });
